6df14091df567c1d0cb77e7084266ed18e025563
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-22 22:55:29 +0800

    Delete junk.py

--- junk.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import pyrax
-
-ctx = pyrax.create_context(env="rax")
-ctx.keyring_auth()
-comp = ctx.get_client("compute", "DFW")
-print "COMP", comp.list()
